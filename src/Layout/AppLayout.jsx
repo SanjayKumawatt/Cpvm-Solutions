422018480db8db577fd68804beac674820be7c84
Sanjay Kumawat <sk9785699310@gmail.com>
2026-01-31 15:37:53 +0530

    message

--- src/Layout/AppLayout.jsx
+++ src/Layout/AppLayout.jsx
@@ -3,7 +3,6 @@ import Header from '../Components/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Components/Footer'
 import ScrollToTop from '../Components/ScrollToTop'
-// import ScrollToTop from "../Components/ScrollToTop"
 
 const AppLayout = () => {
   return (
